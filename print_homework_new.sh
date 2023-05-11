9302112862360735d46bec198e166137f9f669c1
albert <albert.radvan@zslivingston.cz>
2023-05-11 17:22:40 +0200

    fix: error in absolute file path in exclude binary

--- print_homework_new.sh
+++ print_homework_new.sh
@@ -9,4 +9,4 @@ read -r homework
 
 printf "# vim: set syntax=python :\n" >"$directory/homework/$homework.log"
 
-find -P "$directory" -type f -not -path "*/.*" -not -path "$directory/admin*" -not -path "$directory/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*' -exec sh -c 'f="{}"; d="'"$directory"'"; f=${f#$d/}; if file -b "$f" | grep -q text; then printf "Processing %s\n" "$f"; cat "{}"; printf "\n"; fi' \; >>"$directory/homework/$homework.log"
+find -P "$directory" -type f -not -path "*/.*" -not -path "$directory/admin*" -not -path "$directory/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*' -exec sh -c 'f="{}"; d="'"$directory"'"; f=${f#$d/}; if file -b "{}" | grep -q text; then printf "Processing %s\n" "$f"; cat "{}"; printf "\n"; fi' \; >>"$directory/homework/$homework.log"
