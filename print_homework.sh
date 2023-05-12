e7d38171ed406269a9ca3fe9f9cbb9ef40b1a592
albert <albert.radvan@zslivingston.cz>
2023-05-12 12:11:52 +0200

    feat: ignore files in subdirs if also in ./homework

--- print_homework.sh
+++ print_homework.sh
@@ -9,5 +9,4 @@ read -r homework
 
 printf "# vim: set syntax=python :\n" >"$directory/homework/$homework.log"
 
-find -P "$directory" -type f -not -path "*/.*" -not -path "$directory/admin*" -not -path "$directory/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*' -exec sh -c 'f="$1"; d="'"$directory"'"; f=${f#$d/}; fileType=$(file -b "$1"); if echo "$fileType" | grep -q text; then printf "Processing %s\n" "$f"; cat "$1"; printf "\n"; fi' _ {} \; >>"$directory/homework/$homework.log"
-
+find -P "$directory" -type f -not -path "*/.*" -not -path "$directory/admin*" -not -path "$directory/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*' -exec sh -c 'f="$1"; d="'"$directory"'"; f=${f#$d/}; fileName=$(basename "$f"); fileType=$(file -b "$1"); if echo "$fileType" | grep -q text; then if [ ! -f "$d/homework/$fileName" ]; then printf "Processing %s\n" "$f"; cat "$1"; printf "\n"; fi; fi' _ {} \; >>"$directory/homework/$homework.log"
