05c7886c54b21a00354154469095ee4fa65edf2a
albert <albert.radvan@zslivingston.cz>
2023-06-07 12:09:38 +0200

    feat: refactor print_homework.sh, add readline input
    Refactoring script to improve correct var expansion - braces,
    input sanitation - handling dashes in filenames, and runtime -
    - running subshells only in necessary loops.

--- print_homework.sh
+++ print_homework.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-printf "Please enter the absolute path of class directory "
-read -r directory
-mkdir -p "$directory/homework"
+read -e -rp "Please enter the absolute path of class directory: " directory
+directory="${directory//\\ / }"
+mkdir -p "${directory}/homework"
 
-printf "Please enter the name of homework assignment "
-read -r homework
+read -rp "Please enter the name of homework assignment: " homework
 
-printf "# vim: set syntax=python :\n" >"$directory/homework/$homework.log"
+# Create homework .log file and set syntax to python
+printf "# vim: set syntax=python :\n\n" > "${directory}/homework/${homework}.log"
 
 # Store the result of find command into an array
-mapfile -t files < <(find -P "$directory" -type f -not -path "*/.*" -not -path "$directory/admin*" -not -path "$directory/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*')
+mapfile -t files < <(find -P "${directory}" -type f -not -path "*/.*" -not -path "${directory}/admin*" -not -path "${directory}/homework*" -not -path '*/tutor' -not -path '*/tutor.cs*' -not -path '*/TEST*')
 
 # Process each file
 for f in "${files[@]}"; do
-    f=${f#$directory/}
-    fileName=$(basename "$f")
-    fileType=$(file -b "$directory/$f")
+    fileType=$(file -b -- "$f")
     if echo "$fileType" | grep -q text; then
-        if [ ! -f "$directory/homework/$fileName" ]; then
-            printf "Processing %s\n" "$f"
-            cat "$directory/$f"
-            printf "\n"
+    	fileName=$(basename -- "$f")
+        if [ ! -f "${directory}/homework/$fileName" ]; then
+    	    relativePath=$(echo "$f" | sed "s|${directory}/||")
+            printf "\nProcessing # %s\n\n" "$relativePath" >> "${directory}/homework/${homework}.log"
+            cat "$f" >> "${directory}/homework/${homework}.log"
         fi
     fi
-done >>"$directory/homework/$homework.log"
+done
